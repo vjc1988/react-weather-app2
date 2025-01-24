2e321f634d0c2e42ee8b5c8cd213c97a4ef233ee
vjc1988 <victoriajaynecadd@yahoo.com>
2025-01-24 18:18:27 +0000

    added weather app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React, { useState } from "react";
+import "./styles.css";
+import axios from "axios";
 
-function App() {
+export default function App() {
+  let [city, setCity] = useState(null);
+  let [weatherData, setWeatherData] = useState(null);
+
+  function showWeather(response) {
+    setWeatherData(response.data);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a95c2c6739994ba4903e007ee817e7d1`;
+    axios.get(apiURL).then(showWeather);
+  }
+
+  function updateCity(event) {
+    event.preventDefault();
+    setCity(event.target.value);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Weather App</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="search"
+          placeholder="Enter a City..."
+          onChange={updateCity}
+        />
+        <input type="submit" />
+      </form>
+      {weatherData && (
+        <ul>
+          <li>
+            Temperature:
+            {Math.round(weatherData.main.temp)}°C
+          </li>
+          <li>Description: {weatherData.weather[0].description}</li>
+          <li>Humidity: {weatherData.main.humidity}%</li>
+          <li>Wind: {weatherData.wind.speed}km/h</li>
+          <li>
+            <img
+              src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`}
+            />
+          </li>
+        </ul>
+      )}
     </div>
   );
 }
-
-export default App;
