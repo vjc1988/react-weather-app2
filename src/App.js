b264a1c39d9e888e86b991cdfd5dd3f92a05b229
vjc1988 <victoriajaynecadd@yahoo.com>
2025-01-27 10:53:42 +0000

    Added footer with link to Github

--- src/App.js
+++ src/App.js
@@ -49,6 +49,9 @@ export default function App() {
           </li>
         </ul>
       )}
+      <footer>
+        <a href="https://github.com/vjc1988/react-weather-app2" a />
+      </footer>
     </div>
   );
 }
